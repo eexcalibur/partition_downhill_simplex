3423ea6b51dac816cc58d2d0c0fecae872bbcb3f
zhangtao <t-zhang11@mails.tsinghua.edu.cn>
2015-06-23 22:30:48 +0800

    V1.1.3

--- pdso.py
+++ pdso.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 import Queue as Q
 import uq_calibration.config as config
 import uq_calibration.pool as pool
@@ -6,20 +7,17 @@ import uq_calibration.pool as pool
 if __name__ == '__main__':
 	#read the init range
 	init_range = numpy.loadtxt("range_init")
-	
+	para_num   = len(init_range)
+
 	#partition the init range
 	pool_res = pool.Pool()
 	pool_res.set_pool()
-	print pool_res.num_avail_res
+	part_factor = int(math.floor(pool_res.num_avail_res ** 0.5))
+
+	
+	
 
 	q = Q.PriorityQueue()
-	q.put(10)
-	q.put(1)
-	q.put(5)
-	q.put(0.5)
-	while not q.empty():
-		print q.get()
-
 
 
 
